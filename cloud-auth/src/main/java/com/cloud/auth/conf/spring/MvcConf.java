5e679ac6c84a879115b449f432874f81d3c2862e
Damon <554588461@qq.com>
2020-06-29 21:19:00 +0800

    删除无用文件

--- cloud-auth/src/main/java/com/cloud/auth/conf/spring/MvcConf.java
+++ cloud-auth/src/main/java/com/cloud/auth/conf/spring/MvcConf.java
@@ -1,23 +1,23 @@
-package com.cloud.auth.conf.spring;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.MediaType;
-import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-/**
- * Copyright (C),Damon
- *
- * @Description:
- * @Author: Damon(npf)
- * @Date: 2020-06-29:18:06
- */
-@Configuration
-@EnableWebMvc
-public class MvcConf implements WebMvcConfigurer {
-    @Override
-    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-        configurer.defaultContentType(MediaType.APPLICATION_JSON);
-    }
-}
+//package com.cloud.auth.conf.spring;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.MediaType;
+//import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+///**
+// * Copyright (C),Damon
+// *
+// * @Description:
+// * @Author: Damon(npf)
+// * @Date: 2020-06-29:18:06
+// */
+//@Configuration
+//@EnableWebMvc
+//public class MvcConf implements WebMvcConfigurer {
+//    @Override
+//    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+//        configurer.defaultContentType(MediaType.APPLICATION_JSON);
+//    }
+//}
